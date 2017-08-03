e23f47352ec3dece7229afd4801a9fe3baa67db5
Matías Pecchia <matiaspecchia@gridtics.frm.utn.edu.ar>
2017-08-02 23:18:25 -0300

    parsing de SLIP apto para contiki

--- externals/lwip/src/contikibr.c
+++ externals/lwip/src/contikibr.c
@@ -2,11 +2,49 @@
 
 #include "contikibr.h"
 
+
 err_t
 ctk_br_input(struct pbuf *p, struct netif *inp)
 {
-   // FIXME rellenar
-   return ERR_OK;
+   struct pbuf *out;
+   // FIXME hardcoded, debería venir desde otro lugar
+   static u8_t resp_6_addr[10] = {'!', 'P', 0xfd, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+   u16_t i;
+
+   if (p == NULL)
+      return ERR_VAL;
+
+   ciaaPOSIX_printf("VAL: ");
+   ciaaPOSIX_printf("%d ", p->len);
+
+   u8_t * payload = (u8_t *)p->payload;
+
+/*
+   for (i=0 ; i < p->len ; i++)
+       ciaaPOSIX_printf("%x", payload[i]);
+   ciaaPOSIX_printf(" (");
+   for (i=0 ; i < p->len ; i++)
+       ciaaPOSIX_printf("%c", payload[i]);
+   ciaaPOSIX_printf(")\r\n");
+*/
+
+   if (p->len == 2) {
+      if (payload[0] == '?' && payload[1] == 'P') {
+         // alocar pbuf
+         out = pbuf_alloc(PBUF_RAW, 10, PBUF_RAM);
+         out->next = NULL;
+         out->len = 10;
+         out->payload = (void *)&resp_6_addr;
+         // ciaaPOSIX_printf("Enviando direccion");
+         inp->output_ip6(inp, out, (ip6_addr_t *)NULL);
+      }
+      return ERR_OK;
+   }
+   else {
+      return ip_input(p, inp);
+   }
+
+   return ERR_VAL;
 }
 
 
